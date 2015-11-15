de1c702c97f793a2a4a737db48aa23b02cd32f77
andras <kandras@digikabel.hu>
2015-11-15 18:30:36 +0100

    Update utility.java

--- utility.java
+++ utility.java
@@ -4,7 +4,7 @@ package intszol;
 *|*
 *|* Have to be added the following external archive build paths:
 *|*	- mysql-connector-java-[version]-bin.jar
-*|*	- org.eclipse.jdt.annotation_[version].jar														*|
+*|*	- org.eclipse.jdt.annotation_[version].jar
 */
 
 import java.sql.Connection;
@@ -52,8 +52,66 @@ public class utility {
 		
 	}
 
-	
-	 /*Search image
+	 /* Add new image
+	*|* 
+	*|* Expect the parameters of the image:
+	*|*		- user_id
+	*|*		- image name
+	*|*		- date (nullable)
+	*|*		- place (nullable)
+	*|*
+	*|* If date is null, the date will be the sysdate of the MySQL server.
+	*|*
+	*|* Return with the ID of the newly added image.
+	*/
+	public int add_image(int user_id, String name, @Nullable String date, @Nullable String place){
+		
+		int image_id = 0;
+		
+		try {
+			
+			// Add the new image
+			String query = "INSERT INTO image VALUES (null,?,?,";
+			
+			if (date != null)									
+				query += "?,";
+			else
+				query += "sysdate(),";
+			if (place != null)
+				query += "?)";
+			else
+				query += "null)";		
+			
+			java.sql.PreparedStatement stmt = conn.prepareStatement(query);
+			int i=1;
+			stmt.setInt(i, user_id);
+			i++;
+			stmt.setString(i, name);
+			i++;
+			if (date != null)	{ stmt.setString(i, date); 	i++; }
+			if (place != null)	{ stmt.setString(i, place); 	 }
+			
+			stmt.execute();
+			
+			// Query the ID of the newly added image.
+			query = "SELECT * FROM image WHERE user_id = ? AND name = ?";			
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, user_id);
+			stmt.setString(2, name);
+			ResultSet r = stmt.executeQuery();
+			r.next();
+			image_id = r.getInt("id");
+
+		} catch (SQLException ex) {
+			   System.out.println("SQLException: " + ex.getMessage());
+			   System.out.println("SQLState: " + ex.getSQLState());
+			   System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		return image_id;
+	}
+
+	 /* Get image
 	*|* 
 	*|* Expect the parameters of the search:
 	*|*		- user_id
@@ -71,7 +129,7 @@ public class utility {
 	*|*
 	*|* Return with a List<image> list, contain the all metadata of images. 
 	*/ 
-	public List<image> search_image(java.lang.Integer user_id, @Nullable String name, @Nullable String date_from, @Nullable String date_to, @Nullable String place){
+	public List<image> get_image(java.lang.Integer user_id, @Nullable String name, @Nullable String date_from, @Nullable String date_to, @Nullable String place){
 
 		List<image> img_list = new ArrayList<image>();
 		
@@ -154,68 +212,9 @@ public class utility {
 		return img_list;
 	}
 
-	
-	 /* New image
-	*|* 
-	*|* Expect the parameters of the image:
-	*|*		- user_id
-	*|*		- image name
-	*|*		- date (nullable)
-	*|*		- place (nullable)
-	*|*
-	*|* If date is null, the date will be the sysdate of the MySQL server.
-	*|*
-	*|* Return with the ID of the newly added image.
-	*/
-	public int new_image(int user_id, String name, @Nullable String date, @Nullable String place){
-		
-		int image_id = 0;
-		
-		try {
-			
-			// Add the new image
-			String query = "INSERT INTO image VALUES (null,?,?,";
-			
-			if (date != null)									
-				query += "?,";
-			else
-				query += "sysdate(),";
-			if (place != null)
-				query += "?)";
-			else
-				query += "null)";		
-			
-			java.sql.PreparedStatement stmt = conn.prepareStatement(query);
-			int i=1;
-			stmt.setInt(i, user_id);
-			i++;
-			stmt.setString(i, name);
-			i++;
-			if (date != null)	{ stmt.setString(i, date); 	i++; }
-			if (place != null)	{ stmt.setString(i, place); 	 }
-			
-			stmt.execute();
-			
-			// Query the ID of the newly added image.
-			query = "SELECT * FROM image WHERE name = ?";			
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, name);
-			ResultSet r = stmt.executeQuery();
-			r.next();
-			image_id = r.getInt("id");
-
-		} catch (SQLException ex) {
-			   System.out.println("SQLException: " + ex.getMessage());
-			   System.out.println("SQLState: " + ex.getSQLState());
-			   System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		
-		return image_id;
-	}
-
 	 /* Delete image
 	*|* 
-	*|* !!! IF USER_ID AND IMAGE BOTH NULL, ALL IMAGE IS DELETED !!!
+	*|* !!! IF USER_ID AND IMAGE BOTH NULL, ALL IMAGE WILL BE DELETED !!!
 	*|*
 	*|* Expect the parameters of the image:
 	*|*		- user_id
@@ -223,7 +222,7 @@ public class utility {
 	*|*
 	*|* If image is null, all image of the user will be deleted.
 	*|* 
-	*|* !!! IF USER_ID AND IMAGE BOTH NULL, ALL IMAGE IS DELETED !!!
+	*|* !!! IF USER_ID AND IMAGE BOTH NULL, ALL IMAGE WILL BE DELETED !!!
 	*/
 	public void delete_image(java.lang.Integer user_id, @Nullable String name){
 		try {	
@@ -250,6 +249,137 @@ public class utility {
 		}
 	}		
 		
+	 /* Add new comment
+	 |* 
+	 |* Expect the parameters of the comment:
+  	 |* 	- image_id
+  	 |* 	- user_id
+	 |* 	- content
+	 |* 
+	 |* Date will be the sysdate of the MySQL server.
+	*/
+	public void add_comment(int image_id, int user_id, String content){
+		try {
+			
+			// Add the new comment
+			String query = "INSERT INTO comment VALUES (null,?,?,?,sysdate())";
+
+			java.sql.PreparedStatement stmt = conn.prepareStatement(query);
+			
+			stmt.setInt(1, image_id);
+			stmt.setInt(2, user_id);
+			stmt.setString(3, content);
+			
+			stmt.execute();
+			
+		} catch (SQLException ex) {
+			   System.out.println("SQLException: " + ex.getMessage());
+			   System.out.println("SQLState: " + ex.getSQLState());
+			   System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		
+	}
+	
+	 /* Get comment
+	 |* 
+	 |* Expect the parameters of the comment:
+	 |* 	- image_id (nullable)
+	 |* 	- user_id (nullable)
+	 |*
+	 |* Return with a List<comment> list, contain the all metadata of comments.
+	*/
+	public List<comment> get_comment(java.lang.Integer image_id, java.lang.Integer user_id){
+
+		List<comment> cmt_list = new ArrayList<comment>();
+		
+		try {	
+			// Search for the comment(s)
+			String query = "SELECT * FROM comment";
+			if (image_id != null && user_id != null)
+				query += " WHERE image_id = ? AND user_id = ?";
+			if (image_id != null && user_id == null)
+				query += " WHERE image_id = ?";
+			if (image_id == null && user_id != null)
+				query += " WHERE user_id = ?";
+
+			java.sql.PreparedStatement stmt = conn.prepareStatement(query);
+			
+			if (image_id != null && user_id != null){
+				stmt.setInt(1, image_id); 	
+				stmt.setInt(2, user_id); 
+			}
+			if (image_id != null && user_id == null)
+				stmt.setInt(1, image_id); 
+			if (image_id == null && user_id != null)
+				stmt.setInt(1, user_id);
+			
+			// Fill up the list with the image(s)'s ID(s)
+			ResultSet r = stmt.executeQuery();
+				
+			comment cmt = null;
+			while (r.next()){
+				cmt = new comment();
+				cmt.image_id = r.getInt("image_id");
+				cmt.user_id = r.getInt("user_id");
+				cmt.date = r.getString("date");
+				cmt.content = r.getString("content");
+
+				cmt_list.add(cmt);
+			}
+		
+		} catch (SQLException ex) {
+		   System.out.println("SQLException: " + ex.getMessage());
+		   System.out.println("SQLState: " + ex.getSQLState());
+		   System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return cmt_list;
+	}
+	
+	 /* Delete comment
+	 |* 
+	 |* !!! IF IMAGE_ID AND USER_ID BOTH NULL, ALL COMMENT WILL BE DELETED !!!
+	 |*
+	 |* Expect the parameters of the comment:
+	 |* 	- image_id
+	 |* 	- user_id
+	 |*
+	 |* If image_id is null, all comment of the user will be deleted.
+	 |* If user_id is null, all comment of the image will be deleted.
+	 |*
+	 |* !!! IF IMAGE_ID AND USER_ID BOTH NULL, ALL COMMENT WILL BE DELETED !!!
+	*/
+	public void delete_comment(java.lang.Integer image_id, java.lang.Integer user_id){
+		try {
+			// Delete the comment
+			String query = "DELETE FROM comment ";
+			if (image_id != null && user_id != null)
+				query += "WHERE image_id = ? AND user_id = ?";
+			if (image_id != null && user_id == null)
+				query += "WHERE image_id = ?";
+			if (image_id == null && user_id != null)
+				query += "WHERE user_id = ?";
+
+			java.sql.PreparedStatement stmt = conn.prepareStatement(query);
+			
+			if (image_id != null && user_id != null){
+				stmt.setInt(1, image_id); 	
+				stmt.setInt(2, user_id); 
+			}
+			if (image_id != null && user_id == null)
+				stmt.setInt(1, image_id); 
+			if (image_id == null && user_id != null)
+				stmt.setInt(1, user_id);
+
+			stmt.execute();
+			
+		} catch (SQLException ex) {
+		   System.out.println("SQLException: " + ex.getMessage());
+		   System.out.println("SQLState: " + ex.getSQLState());
+		   System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}		
+	
 }
 
 
