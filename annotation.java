3684ad54db2484ed08a1e87dff028ffb4555e447
andras <kandras@digikabel.hu>
2015-11-15 21:19:47 +0100

    Create annotation.java

---
+++ annotation.java
@@ -0,0 +1,10 @@
+package intszol;
+
+class annotation {
+	public int image_id;
+	public String content;
+	
+    public String toString() {    	
+        return image_id + "\t" + "\t" + content;
+    }
+}
