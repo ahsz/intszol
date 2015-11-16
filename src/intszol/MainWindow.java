02d6404c5004c02e011b83bc660deed57a15019f
tuba59 <barnabas.turi@gmail.com>
2015-11-16 17:36:34 +0100

    Main osztály törölve, helyette MainWindow
    Main osztály tartalma áthelyezve a MainWindow osztályba, ahol az ablak
    létre lesz hozva, ha valamit akartok a mainbe rakni akkor ide tegyétek.

--- src/intszol/MainWindow.java
+++ src/intszol/MainWindow.java
@@ -1,12 +1,30 @@
 package intszol;
 
+import java.awt.EventQueue;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		Utility ut = new Utility();
 		int i;
 		
@@ -117,6 +135,22 @@ public class Main {
 		}
 		
 		System.out.print("\n" + "\n");
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
