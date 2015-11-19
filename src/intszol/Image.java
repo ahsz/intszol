1e9768bab645a257a60e97846b43b88404fdea76
andras <kandras@digikabel.hu>
2015-11-19 22:29:16 +0100

    Update Image.java

--- src/intszol/Image.java
+++ src/intszol/Image.java
@@ -8,9 +8,11 @@ class Image {
 	public String place;
 	public String gd_id;
 	public String gd_url;
+	public String gd_id2;
+	public String gd_url2;
 	
     public String toString() {
     	
-        return id + "\t" + user_id + "\t" + name + "\t" + date + "\t" + place + "\t" + gd_id + "\t" + gd_url;
+        return id + "\t" + user_id + "\t" + name + "\t" + date + "\t" + place + "\t" + gd_id + "\t" + gd_url + "\t" + gd_id2 + "\t" + gd_url2;
     }
 }
